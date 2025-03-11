886c23aa61cf121d348be76d193febba75029cb8
ahmed elmelegy <ahmedelmelegy3570@gmail.com>
2025-03-11 23:49:50 +0200

    feat(ecr): add ecr and registry policy

---
+++ ecr.tf
@@ -0,0 +1,29 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+resource "aws_ecr_repository" "apps_ecr" {
+  name                 = "apps_ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_registry_policy" "example" {
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowPublicFullAccess",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "ecr:*",
+        Resource  = "arn:${data.aws_partition.current.partition}:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/*"
+      }
+    ]
+  })
+}
